98363ea481da7795b1e3f516bd54140a9da12d40
Markus Schmidt <schmidt@boomshop.net>
2013-03-20 18:12:36 +0100

    fake on-air

--- javascript/operator.js
+++ javascript/operator.js
@@ -72,7 +72,7 @@ OperatorUI = new Class({
         this._resize();
         
         // fake ON-AIR
-        this.on_air.element.addEvent("touchend mouseup", function (e) { e.stopPropagation(); e.preventDefault(); this.on_air.set("state", !this.on_air.get("state")) }.bind(this));
+        this.on_air.element.addEvent("mouseup", function (e) { e.stopPropagation(); e.preventDefault(); this.on_air.set("state", !this.on_air.get("state")) }.bind(this));
     },
     
     _resize: function () {
