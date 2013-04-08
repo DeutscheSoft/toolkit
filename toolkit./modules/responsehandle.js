1ba2bd583d6a20bc95828f020c132c713a5f2ca8
Markus Schmidt <schmidt@boomshop.net>
2013-04-09 01:38:43 +0200

    response handle with gesture z

--- toolkit./modules/responsehandle.js
+++ toolkit./modules/responsehandle.js
@@ -221,7 +221,7 @@ ResponseHandle = new Class({
             l.set({x:0, dy:"1.0em"});
         }
         this._label.set("html", t);
-        $("log").set("text", this._label.getChildren()[0].get("dy"));
+        $("log").set("text", this._label.getChildren()[0].get("x"));
         
         switch(this.options.mode) {
             case _TOOLKIT_CIRCULAR:
