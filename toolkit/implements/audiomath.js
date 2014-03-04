2d067a4d2f2d90439f8e1ec896535924aa4533da
Arne Goedeke <el@laramies.com>
2014-03-04 09:45:04 -0500

    make scaled versions inlineable (i.e. do not call methods in this)

--- toolkit/implements/audiomath.js
+++ toolkit/implements/audiomath.js
@@ -30,7 +30,7 @@ AudioMath = new Class({
         var logfac = Math.max(1, Math.pow(2, factor) - 1);
         if (reverse) value = max - (value - min);
         value = Math.log2(1 + (value - min) / (max - min) * logfac) / factor;
-        if (reverse) return -value + 1;
+        if (reverse) value = -value + 1;
         return value;
     },
     coef2db: function (coef, min, max, reverse, factor) {
@@ -38,14 +38,25 @@ AudioMath = new Class({
         var logfac = Math.max(1, Math.pow(2, factor) - 1);
         if (reverse) coef = -coef + 1;
         coef = (Math.pow(2, coef * factor) - 1) / logfac * (max - min) + min;
-        if (reverse) return max - coef + min;
-        return coef
+        if (reverse) coef = max - coef + min;
+        return coef;
     },
     db2scale: function (value, min, max, scale, reverse, factor) {
-        return this.db2coef(value, min, max, reverse, factor) * scale;
+        factor = factor || 1;
+        var logfac = Math.max(1, Math.pow(2, factor) - 1);
+        if (reverse) value = max - (value - min);
+        value = Math.log2(1 + (value - min) / (max - min) * logfac) / factor;
+        if (reverse) value = -value + 1;
+        return value * scale;
     },
     scale2db: function (value, min, max, scale, reverse, factor) {
-        return this.coef2db(value / scale, min, max, reverse, factor);
+        value /= scale;
+        factor = factor || 1;
+        var logfac = Math.max(1, Math.pow(2, factor) - 1);
+        if (reverse) value = -value + 1;
+        value = (Math.pow(2, value * factor) - 1) / logfac * (max - min) + min;
+        if (reverse) value = max - value + min;
+        return value;
     },
     // FREQUENCY CALCULATIONS
     freq2coef: function (value, min, max, reverse, prescaled, factor) {
@@ -53,7 +64,7 @@ AudioMath = new Class({
         min   = Math.log10(min);
         max   = Math.log10(max);
         value = ((Math.log10(value) - min) / (max - min));
-        if (reverse) return -value + 1;
+        if (reverse) value = -value + 1;
         return value;
     },
     coef2freq: function (coef, min, max, reverse) {
@@ -61,13 +72,24 @@ AudioMath = new Class({
         min  = Math.log10(min);
         max  = Math.log10(max);
         coef = Math.pow(10, (coef * (max - min) + min));
-        if (reverse) return max - coef + min;
+        if (reverse) coef = max - coef + min;
         return coef
     },
     freq2scale: function (value, min, max, scale, reverse) {
-        return this.freq2coef(value, min, max, reverse) * scale;
+        if (reverse) value = max - (value - min);
+        min   = Math.log10(min);
+        max   = Math.log10(max);
+        value = ((Math.log10(value) - min) / (max - min));
+        if (reverse) value = -value + 1;
+        return value * scale;
     },
     scale2freq: function (value, min, max, scale, reverse) {
-        return this.coef2freq(value / scale, min, max, reverse);
+        value /= scale;
+        if (reverse) value = -value + 1;
+        min  = Math.log10(min);
+        max  = Math.log10(max);
+        value = Math.pow(10, (value * (max - min) + min));
+        if (reverse) value = max - value + min;
+        return value;
     }
 });
