d316ed8e694c907b8c627cb5f1ed5a8a88aefb12
Markus Schmidt <schmidt@boomshop.net>
2015-07-24 22:55:28 +0200

    Initial replacement of mootools drag Still broken!

--- toolkit/modules/drag.js
+++ toolkit/modules/drag.js
@@ -19,16 +19,14 @@
  * Boston, MA  02110-1301  USA
  */
 
-Resize = $class({
+Drag = $class({
     // Resize enables resizing of elements on the screen.
-    _class: "Resize",
+    _class: "Drag",
     Extends: Widget,
     options: {
         element   : null,           // the element to resize
         handle    : null,           // a DOM node used as handle. if none set
                                     // element is used
-        direction : _TOOLKIT_SE,    // _TOOLKIT_N, _TOOLKIT_S, _TOOLKIT_E, _TOOLKIT_W,
-                                    // _TOOLKIT_NE, _TOOLKIT_SE, _TOOLKIT_SW, _TOOLKIT_NW,
         active    : true,           // set to false if resize is disabled
         min       : {x: -1, y: -1}, // object containing x and y determining minimum size
                                     // a value of -1 means no min
@@ -56,8 +54,6 @@ Resize = $class({
         if (!this.options.active)
         this._xstart = e.pageX;
         this._ystart = e.pageY;
-        this._xsize  = this.options.element.offsetWidth;
-        this._ysize  = this.options.element.offsetHeight;
         this._xpos   = this.options.element.offsetLeft;
         this._ypos   = this.options.element.offsetTop;
         if (!this.options.active)
@@ -72,20 +68,20 @@ Resize = $class({
     _dragging: function (e, drag) {
         if (!this.options.active)
             return;
-        var w = this._xsize + e.pageX - this._xstart;
-        var h = this._ysize + e.pageY - this._ystart;
+        var x = this._xpos + e.pageX - this._xstart;
+        var y = this._ypos + e.pageY - this._ystart;
         if (this.options.min.x >= -1)
-            w = Math.max(this.options.min.x, w);
+            x = Math.max(this.options.min.x, x);
         if (this.options.max.x >= -1)
-            w = Math.min(this.options.max.x, w);
+            x = Math.min(this.options.max.x, x);
         if (this.options.min.y >= -1)
-            h = Math.max(this.options.min.y, h);
+            y = Math.max(this.options.min.y, y);
         if (this.options.max.y >= -1)
-            h = Math.min(this.options.max.y, h);
-        this.options.element.style.width = w + "px";
-        this.options.element.style.height = h + "px";
+            y = Math.min(this.options.max.y, y);
+        this.options.element.style.top = y + "px";
+        this.options.element.style.left = x + "px";
         
-        this.fire_event("resizing", [e, w, h, this]);
+        this.fire_event("resizing", [e, x, y, this]);
     },
     // GETTERS & SETTERS
     set: function (key, value, hold) {
@@ -94,8 +90,6 @@ Resize = $class({
             case "handle":
                 if (!value)
                     this.options.handle = this.options.element;
-            case "handle":
-            case "direction":
                 if (!hold)
                     this._set_handle();
                 break;
--- toolkit/widgets/window.js
+++ toolkit/widgets/window.js
@@ -186,28 +186,28 @@ Window = $class({
         this.element.addEventListener("mouseleave", this.__mout.bind(this));
         this._header.addEventListener("dblclick", this.__header_action.bind(this));
         
-        this.drag = false; //new Drag(this.element, {
-            //handle: this._header,
-            //onStart: this.__start_drag.bind(this),
-            //onComplete: this.__stop_drag.bind(this),
-            //onDrag: this.__dragging.bind(this),
-            //limit: {x: [0 - this.options.width + 20,
-                        //window.getSize().x - 20],
-                    //y: [0, window.getSize().y - 20]}
-        //});
+        this.drag = new Drag({
+            element    : this.element,
+            handle     : this._header,
+            onStart    : this.__start_drag.bind(this),
+            onStop     : this.__stop_drag.bind(this),
+            onDragging : this.__dragging.bind(this),
+            min        : {x: 0 - this.options.width + 20, y: 0},
+            max        : {x: window.getSize().x - 20, y: window.getSize().y - 20}
+        });
         
         this._resize = toolkit.element("div", "toolkit-resize");
         this.element.appendChild(this._resize);
         
         this.resize = new Resize({
-            element: this.element,
-            handle: this._resize,
-            min: {x: this.options.min_width, y: this.options.min_height},
-            max: {x: this.__max_width(), y: this.__max_height()}
+            element    : this.element,
+            handle     : this._resize,
+            min        : {x: this.options.min_width, y: this.options.min_height},
+            max        : {x: this.__max_width(), y: this.__max_height()},
+            onStart    : this.__start_resize.bind(this),
+            onStop     : this.__stop_resize.bind(this),
+            onResizing : this.__resizing.bind(this)
         });
-        this.resize.add_event("start",    this.__start_resize.bind(this));
-        this.resize.add_event("stop", this.__stop_resize.bind(this));
-        this.resize.add_event("resizing", this.__resizing.bind(this));
         
         this.set("resizable", this.options.resizable);
         this.set("draggable", this.options.draggable);
@@ -494,12 +494,12 @@ Window = $class({
         if (this.__horiz_max()) this.options.x = pos.x;
         if (this.__vert_max())  this.options.y = pos.y;
         
-        this.drag.value.now.x += x;
-        this.drag.mouse.now.x += x;
-        this.drag.mouse.pos.x -= x;
-        this.drag.value.now.y += y;
-        this.drag.mouse.now.y += y;
-        this.drag.mouse.pos.y -= y;
+        //this.drag.value.now.x += x;
+        //this.drag.mouse.now.x += x;
+        //this.drag.mouse.pos.x -= x;
+        //this.drag.value.now.y += y;
+        //this.drag.mouse.now.y += y;
+        //this.drag.mouse.pos.y -= y;
         
         // un-maximize
         if (this.__horiz_max()) this.set("maximize", {x: false});
