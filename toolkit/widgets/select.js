c3fafeb3c3e8ddb29431b60fd1538043cebdd8fd
Arne Goedeke <el@laramies.com>
2015-10-09 19:16:00 +0200

    Select: refactored rendering
    This also moves some private methods out of the prototype.
    Fixed #17

--- toolkit/widgets/select.js
+++ toolkit/widgets/select.js
@@ -20,6 +20,82 @@
  */
 "use strict";
 (function(w){
+function index_by_value(val) {
+    var entries = this.entries;
+    for (var i = 0; i < entries.length; i++) {
+        if (entries[i].value === val)
+            return i;
+    }
+    return false;
+}
+function index_by_title(title) {
+    var entries = this.entries;
+    for (var i = 0; i < entries.length; i++) {
+        if (entries[i].title === title)
+            return i;
+    }
+    return false;
+}
+function index_by_entry(entry) {
+    var pos = this.entries.indexOf(entry);
+    return pos === -1 ? false : pos;
+}
+function hide_list() {
+    this.__transition = false;
+    this.__timeout = false;
+    if (!this.__open) {
+        TK.destroy(this._list);
+    } else {
+        document.addEventListener("touchstart", this._global_touch_start);
+        document.addEventListener("mousedown", this._global_touch_start);
+    }
+}
+function set_size() {
+    // set all possible titles into the buttons label, measure the
+    // max width and set it as style afterwards
+    /* FORCE_RELAYOUT */
+    this.__width = 0;
+    this.element.style.width = "auto";
+    var t = this._label.innerHTML;
+    for (var i = 0; i < this.entries.length; i++) {
+        this.set("label", this.entries[i].title);
+        var act = TK.outer_width(this.element, true);
+        this.__width = Math.max(this.__width, act);
+    }
+    TK.outer_width(this.element, true, this.__width);
+    this._label.innerHTML = t;
+}
+function show_list(show) {
+    if (show) {
+        var ew = TK.outer_width(this.element, true);
+        document.body.appendChild(this._list);
+        var cw = TK.width();
+        var ch = TK.height();
+        var sx = TK.scroll_left();
+        var sy = TK.scroll_top();
+        TK.set_styles(this._list, {
+            "opacity": 0,
+            "maxHeight": ch,
+            "maxWidth": cw,
+            "minWidth": ew
+        });
+        var lw = TK.outer_width(this._list, true);
+        var lh = TK.outer_height(this._list, true);
+        TK.set_styles(this._list, {
+            "top": Math.min(TK.position_top(this.element) + TK.outer_height(this.element, true), ch + sy - lh) + "px",
+            "left": Math.min(TK.position_left(this.element), cw + sx - lw) + "px",
+        });
+    } else {
+        document.removeEventListener("touchstart", this._global_touch_start);
+        document.removeEventListener("mousedown", this._global_touch_start);
+    }
+    TK.set_style(this._list, "opacity", show ? "1" : "0");
+    this.__transition = true;
+    this.__open = show;
+    if (this.__timeout !== false) window.clearTimeout(this.__timeout);
+    var dur = parseFloat(TK.get_style(this._list, "transition-duration"));
+    this.__timeout = window.setTimeout(hide_list.bind(this), dur * 1000);
+}
 w.Select = $class({
     // Select provides a button with a select list to choose from
     // different entries.
@@ -36,11 +112,12 @@ w.Select = $class({
         this.__timeout = -1;
         this.__width = 0;
         this.entries = [];
+        this._active = null;
         Button.prototype.initialize.call(this, options);
         TK.add_class(this.element, "toolkit-select");
         
         this.add_event("click", function (e) {
-            this.show_list(!this.__open);
+            show_list.call(this, !this.__open);
         }.bind(this));
         
         this._list = TK.element("ul", "toolkit-select-list");
@@ -49,7 +126,7 @@ w.Select = $class({
                 !this._list.contains(e.target) &&
                 !this.element.contains(e.target)) {
 
-                this.show_list(false);
+                show_list.call(this, false);
             }
         }.bind(this);
         this._arrow = TK.element("div", "toolkit-arrow");
@@ -70,48 +147,26 @@ w.Select = $class({
     },
     
     select: function (id) {
-        // Select an entry. Hand over the ID (position in the list) or
-        // false if nothing should be selected.            
-        var sel = this.options.selected;
-        this.options.selected = id;
-        
-        // remove active style from last selected
-        if (sel !== false && sel >= 0 && sel < this.entries.length)
-            TK.remove_class(this.entries[sel].element, "toolkit-active");
-        
-        if (id !== false && id >= 0 && id < this.entries.length) {
-            // add active style to selection
-            TK.add_class(this.entries[id].element, "toolkit-active");
-            // and set the selected value to options
-            this.options.value = this.entries[id].value;
-            // label
-            this.set("label", this.entries[id].title);
-        } else {
-//          this.options.value = false;
-            this.set("label", "");
-        }
+        this.set("selected", id);
     },
     
     select_value: function (value) {
-        var id = this._get_entry_by_value(value, true);
-        if (id !== false || value === false)
-            this.select(id);
+        var id = index_by_value.call(this, value);
+        this.set("selected", id);
     },
     
     set_entries: function (entries) {
         // Replace all entries with a new options list
         this.clear();
         this.add_entries(entries);
-        this.select(this._get_entry_by_value(this.options.value, true));
+        this.select(index_by_value.call(this, this.options.value));
     },
     add_entries: function (entries) {
         for (var i = 0; i < entries.length; i++)
             this.add_entry(entries[i], true);
-        this.set_size();
     },
     add_entry: function (ent, hold) {
         var li = TK.element("li", "toolkit-option");
-        this._list.appendChild(li);
         var entry = {};
         entry.element = li;
         entry.value = (typeof ent == "string") ? ent
@@ -120,157 +175,118 @@ w.Select = $class({
                        ? ent : (typeof ent.title != "undefined")
                        ? ent.title : ent.value.toString()
         
-        li.innerHTML = entry.title;
+        TK.set_text(li, entry.title);
         
         this.entries.push(entry);
         var id = this.entries.length - 1;
         var up_cb = function (e) {
             this.select(id);
             this.fire_event("select", entry.value, id, entry.title);
-            this.show_list(false);
+            show_list.call(this, false);
         }.bind(this);
+
         li.addEventListener("click", up_cb);
+
+        this.invalid.entries = true;
+
         if (this.options.selected === id) {
-            this.select(id);
-        } 
-        if (this.options.selected > id) {
-            this.select(this.options.selected+1);
+            this.invalid.selected = true;
+            this.trigger_draw();
+        } else if (this.options.selected > id) {
+            this.set("selected", this.options.selected+1);
+        } else {
+            this.trigger_draw();
         }
-        
-        if (!hold) this.set_size();
+
+        this._list.appendChild(li);
     },
     remove_value: function (val) {
-        this.remove_id(this._get_entry_by_value(val, true));
+        this.remove_id(index_by_value.call(this, val));
     },
     remove_title: function (title) {
-        this.remove_id(this._get_entry_by_title(title, true));
+        this.remove_id(index_by_title.call(this, title));
     },
     remove_entry: function (entry) {
-        this.remove_id(this._get_entry(entry, true));
+        this.remove_id(get_entry.call(this, entry));
     },
     remove_id: function (id) {
-        // in range?
-        if (id === false || id < 0 || id >= this.options.length)
-            return;
         // remove DOM element
-        this.entries[id].element.parentNode.removeChild(this.entries[id].element);
-        // remove from list
-        this.entries.splice(id, 1);
-        // selection
-        var sel = this.options.selected;
-        if (sel !== false && sel > id)
-            this.options.selected --;
-        else if (sel !== false && sel == id) {
-            this.options.selected = false;
-            this.set("label", "");
+        var entry = this.entries[id];
+
+        if (entry) {
+            var li = entry.element;
+            this._list.removeChild(li);
+            // remove from list
+            this.entries.splice(id, 1);
+            // selection
+            var sel = this.options.selected;
+            if (sel !== false) {
+                if (sel > id) {
+                    this.options.selected --;
+                } else if (sel === id) {
+                    this.options.selected = false;
+                    this.set("label", "");
+                }
+            }
+            this.invalid.entries = true;
+            this.select(this.options.selected);
         }
-        this.select(this.options.selected)
     },
     clear: function () {
-        this.set("label", "");
-        this._list.innerHTML = "";
+        TK.empty(this._list);
         this.select(false);
         this.entries = [];
     },
-    set_size: function () {
-        if (!this.options.auto_size)
-            return;
-        // set all possible titles into the buttons label, measure the
-        // max width and set it as style afterwards
-        /* FORCE_RELAYOUT */
-        this.__width = 0;
-        this.element.style.width = "auto";
-        var t = this._label.innerHTML;
-        for (var i = 0; i < this.entries.length; i++) {
-            this.set("label", this.entries[i].title);
-            var act = TK.outer_width(this.element, true);
-            this.__width = Math.max(this.__width, act);
+
+    redraw: function() {
+        Button.prototype.redraw.call(this);
+
+        var I = this.invalid;
+        var O = this.options;
+
+        if (I.validate("entries", "auto_size")) {
+            if (O.auto_size) set_size.call(this);
         }
-        TK.outer_width(this.element, true, this.__width);
-        this._label.innerHTML = t;
-    },
-    
-    show_list: function (show) {
-        if (show) {
-            var ew = TK.outer_width(this.element, true);
-            document.body.appendChild(this._list);
-            var cw = TK.width();
-            var ch = TK.height();
-            var sx = TK.scroll_left();
-            var sy = TK.scroll_top();
-            TK.set_styles(this._list, {
-                "opacity": 0,
-                "maxHeight": ch,
-                "maxWidth": cw,
-                "minWidth": ew
-            });
-            var lw = TK.outer_width(this._list, true);
-            var lh = TK.outer_height(this._list, true);
-            TK.set_styles(this._list, {
-                "top": Math.min(TK.position_top(this.element) + TK.outer_height(this.element, true), ch + sy - lh) + "px",
-                "left": Math.min(TK.position_left(this.element), cw + sx - lw) + "px",
-            });
-        } else {
-            document.removeEventListener("touchstart", this._global_touch_start);
-            document.removeEventListener("mousedown", this._global_touch_start);
-        }
-        TK.set_style(this._list, "opacity", show ? "1" : "0");
-        this.__transition = true;
-        this.__open = show;
-        if (this.__timeout !== false) window.clearTimeout(this.__timeout);
-        var dur = parseFloat(TK.get_style(this._list, "transition-duration"));
-        this.__timeout = window.setTimeout(this._hide_list.bind(this), dur * 1000);
-    },
-    _hide_list: function () {
-        this.__transition = false;
-        this.__timeout = false;
-        if (!this.__open) {
-            TK.destroy(this._list);
-        } else {
-            document.addEventListener("touchstart", this._global_touch_start);
-            document.addEventListener("mousedown", this._global_touch_start);
+
+        if (I.selected) {
+            if (this._active) {
+                TK.remove_class(this._active, "toolkit-active");
+            }
+            var entry = this.entries[O.selected];
+
+            if (entry) {
+                this._active = entry.element;
+                TK.add_class(entry.element, "toolkit-active");
+            } else {
+                this._active = null;
+            }
         }
     },
-    _get_entry_by_value: function (val, id) {
-        for (var i = 0; i < this.entries.length; i++) {
-            if (this.entries[i].value === val)
-                return id ? i : this.entries[i];
-        }
-        return false;
-    },
-    _get_entry_by_title: function (title, id) {
-        for (var i = 0; i < this.entries.length; i++) {
-            if (this.entries[i].title === title)
-                return id ? i : this.entries[i];
-        }
-        return false;
-    },
-    _get_entry: function (option, id) {
-        for (var i = 0; i < this.entries.length; i++) {
-            if (this.entries[i] === option)
-                return id ? i : this.entries[i];
-        }
-        return false;
+    current: function() {
+        return this.entries[this.options.selected];
     },
     set: function (key, value, hold) {
+        if (key === "value") {
+            var index = index_by_value.call(this, value);
+            if (index === false) return;
+            key = "selected";
+            value = index;
+        }
+        Button.prototype.set.call(this, key, value, hold);
         switch (key) {
             case "selected":
-                this.select(value);
+                var entry = this.current();
+                if (entry) {
+                    Button.prototype.set.call(this, "value", entry.value); 
+                    this.set("label", entry.title);
+                } else {
+                    this.set("label", "");
+                }
                 break;
-            case "value":
-                this.select_value(value);
-                break;
-        }
-        this.options[key] = value;
-        switch (key) {
             case "entries":
                 this.set_entries(value);
                 break;
-            case "auto_size":
-                this.set_size();
-                break;
         }
-        Button.prototype.set.call(this, key, value, hold);
     }
 });
 })(this);
