791593fae6499db589d3f199d9220f98944102dd
Arne Goedeke <el@laramies.com>
2015-10-16 16:20:04 +0200

    Widget.fire_event(): removed unused code

--- toolkit/widgets/widget.js
+++ toolkit/widgets/widget.js
@@ -92,7 +92,6 @@ w.Widget = $class({
         BASE.prototype.fire_event.apply(this, arguments);
 
         if (this.shared_events[type] && this.children.length) {
-            var args = [ this ].concat(arguments);
             c = this.children;
             for (i = 0; i < c.length; i++) {
                 c[i].fire_event.apply(c[i], arguments);
