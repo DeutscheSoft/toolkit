371a4046fb545bbb4412b4d9b8985127a089d50d
Arne Goedeke <el@laramies.com>
2016-02-22 15:04:44 +0100

    Expander: added 'always_expanded' option
    the 'always_expanded' option does exactly that. It can be used to
    set an expander to be fully expanded for instance when using an
    expander with media-events.

--- toolkit/widgets/expander.js
+++ toolkit/widgets/expander.js
@@ -31,6 +31,45 @@ function collapse(state, e) {
     this.set("expanded", state);
     return false;
 }
+function visible_when_expanded(widget) {
+    var v = widget.options._expanded;
+    return v !== false;
+}
+function visible_when_collapsed(widget) {
+    var v = widget.options._collapsed;
+    return v === true;
+}
+function is_visible(widget) {
+    var value = this.options.always_expanded || this.options.expanded;
+
+    if (value) {
+        return visible_when_expanded(widget);
+    } else {
+        return visible_when_collapsed(widget);
+    }
+}
+function update_visibility() {
+    var C = this.children;
+    var value = this.options.always_expanded || this.options.expanded;
+
+    if (value) {
+        for (var i = 0; i < C.length; i++) {
+            if (visible_when_expanded(C[i]))
+                this.show_child(i);
+            else
+                this.hide_child(i);
+        }
+        this.fire_event("expand");
+    } else {
+        for (var i = 0; i < C.length; i++) {
+            if (visible_when_collapsed(C[i]))
+                this.show_child(i);
+            else
+                this.hide_child(i);
+        }
+        this.fire_event("collapse");
+    }
+}
 w.TK.Expander = w.Expander = $class({
     /**
      * TK.Expander is a container able to expand to a fixed fullscreen view.
@@ -45,9 +84,11 @@ w.TK.Expander = w.Expander = $class({
     _class: "Expander",
     _options: Object.assign(Object.create(TK.Container.prototype._options), {
         expanded: "boolean",
+        always_expanded: "boolean",
     }),
     options: {
         expanded: false,
+        always_expanded: false,
     },
     Extends: TK.Container,
     toggle: function() {
@@ -59,9 +100,10 @@ w.TK.Expander = w.Expander = $class({
 
         TK.Container.prototype.redraw.call(this);
 
-        if (I.expanded) {
-            I.expanded = false; 
-            this[O.expanded ? "add_class" : "remove_class"]("toolkit-expanded");
+        if (I.expanded || I.always_expanded) {
+            I.always_expanded = I.expanded = false; 
+            var v = O.always_expanded || O.expanded;
+            this[v ? "add_class" : "remove_class"]("toolkit-expanded");
             TK.S.after_frame(this.resize.bind(this));
         }
     },
@@ -73,29 +115,22 @@ w.TK.Expander = w.Expander = $class({
             container: this.element,
             "class": "toolkit-toggle-expand"
         });
+        this._update_visibility = update_visibility.bind(this);
         this.add_event("hide", collapse.bind(this, false));
+        this.add_event("set_expanded", update_visibility);
+        this.add_event("set_always_expanded", update_visibility);
         this.set("expanded", this.options.expanded);
+        this.set("always_expanded", this.options.always_expanded);
     },
-    set: function(key, value) {
-        var O = this.options;
-        if (key === "expanded" && value !== O.expanded) {
-            var C = this.children;
-            if (value) {
-                for (var i = 0; i < C.length; i++) {
-                    this.show_child(i);
-                }
-                this.fire_event("expand");
-            } else {
-                for (var i = 0; i < C.length; i++) {
-                    if (!C[i].options._fixed) {
-                        this.hide_child(i);
-                    }
-                }
-                this.fire_event("collapse");
-            }
-        }
-
-        TK.Container.prototype.set.call(this, key, value);
-    }
+    add_child: function(child) {
+        TK.Container.prototype.add_child.call(this, child);
+        if (!is_visible.call(this, child)) this.hide_child(child);
+        child.add_event("set__expanded", this._update_visibility);
+        child.add_event("set__collapsed", this._update_visibility);
+    },
+    remove_child: function(child) {
+        child.remove_event("set__expanded", this._update_visibility);
+        child.remove_event("set__collapsed", this._update_visibility);
+    },
 });
 })(this);
