c0137cae513b7f47be8ede66515c4593fd89ecbf
Arne Goedeke <el@laramies.com>
2016-10-12 18:43:33 +0200

    Toggle: improved touch handling
    * correctly handles several touch points
    * no need to use setTimeout for the press timeout

--- toolkit/widgets/toggle.js
+++ toolkit/widgets/toggle.js
@@ -24,41 +24,93 @@
  */
 "use strict";
 (function(w){ 
-function mousedown(e) {
-    if (this.__toggleclick) return;
-    this.__toggleclick = this.__toggledown = true;
-    if (this.options.press) {
-        this.toggle();
-        this.fire_event("useraction", "state", this.options.state);
-        this.__toto = window.setTimeout(function () {
-            this.__toggleclick = false;
-        }.bind(this), this.options.press);
-    }
-    e.preventDefault();
-    return false;
+
+/* Abstract toggle logic */
+
+function toggle(O) {
+    var state = !O.state;
+    state = this.set("state", state);
+    this.fire_event("useraction", "state", state);
 }
-function mouseup(e) {
-    if (!this.__toggledown) return;
+function press_start() {
     var O = this.options;
-    this.__toggledown = false;
-    if ((O.press && (this.__toggleclick && O.toggle))
-    || (!O.press && !O.toggle)) {
-        // do not toggle
-        clear_to.call(this);
-        this.__toggleclick = false;
-        return;
+    this.__press_start_time = Date.now();
+
+    if (O.press) toggle.call(this, O);
+}
+function press_end() {
+    var O = this.options;
+    var t = Date.now() - this.__press_start_time;
+
+    if ((O.toggle && (!O.press || t > O.press)) || (!O.toggle && O.press)) {
+        toggle.call(this, O);
     }
-    this.toggle();
-    this.fire_event("useraction", "state", this.options.state);
-    this.__toggleclick = false;
-    e.preventDefault();
+}
+function press_cancel() {
+    var O = this.options;
+
+    /* this is definitely not a click, its a cancel */
+
+    if (O.press) toggle.call(this, O);
+}
+
+/* MOUSE handling */
+function mouseup(e) {
+    this.remove_event("mouseup", mouseup);
+    this.remove_event("mouseleave", mouseleave);
+    press_end.call(this);
+}
+function mouseleave(e) {
+    this.remove_event("mouseup", mouseup);
+    this.remove_event("mouseleave", mouseleave);
+    press_cancel.call(this);
+}
+function mousedown(e) {
+    /* only left click please */
+    if (e.button) return true;
+    press_start.call(this);
+    this.add_event("mouseup", mouseup);
+    this.add_event("mouseleave", mouseleave);
+}
+
+/* TOUCH handling */
+function is_current_touch(ev) {
+    var id = this.__touch_id;
+    var i;
+    for (i = 0; i < ev.changedTouches.length; i++) {
+        if (ev.changedTouches[i].identifier === id) {
+            return true;
+        }
+    }
+
     return false;
 }
-function clear_to() {
-    if (this.__toto) {
-        window.clearTimeout(this.__toto);
-        this.__toto = null;
-    }
+
+function touchend(e) {
+    if (!is_current_touch.call(this, e)) return;
+    this.__touch_id = false;
+    press_end.call(this);
+
+    this.remove_event("touchend", touchend);
+    this.remove_event("touchcancel", touchleave);
+    this.remove_event("touchleave", touchleave);
+}
+function touchleave(e) {
+    if (!is_current_touch.call(this, e)) return;
+    this.__touch_id = false;
+    press_cancel.call(this);
+
+    this.remove_event("touchend", touchend);
+    this.remove_event("touchcancel", touchleave);
+    this.remove_event("touchleave", touchleave);
+}
+function touchstart(e) {
+    if (this.__touch_id !== false) return;
+    this.__touch_id = e.targetTouches[0].identifier;
+    press_start.call(this);
+    this.add_event("touchend", touchend);
+    this.add_event("touchcancel", touchleave);
+    this.add_event("touchleave", touchleave);
 }
     
 w.TK.Toggle = w.Toggle = $class({
@@ -103,8 +155,11 @@ w.TK.Toggle = w.Toggle = $class({
     initialize: function (options) {
         TK.Button.prototype.initialize.call(this, options);
         TK.add_class(this.element, "toolkit-toggle");
-        this.add_event("pointerdown", mousedown);
-        this.add_event("pointerup", mouseup);
+        this.add_event("mousedown", mousedown);
+        this.add_event("touchstart", touchstart, true, true);
+        this.add_event("contextmenu", function(ev) {}, true, true);
+        this.__press_start_time = 0;
+        this.__touch_id = false;
     },
     
     redraw: function () {
@@ -148,7 +203,6 @@ w.TK.Toggle = w.Toggle = $class({
      */
     toggle: function () {
         var state = !this.options.state;
-        clear_to.call(this);
         this.set("state", state);
         /**
          * Is fired when the toggle button was clicked by the user.
@@ -162,15 +216,6 @@ w.TK.Toggle = w.Toggle = $class({
          * @event TK.Toggle#useraction
          */
         this.fire_event("useraction", "state", state);
-    },
-    /* INTERNAL */
-    cancel_press: function () {
-        if (!this.__tp) return;
-        this.__tp = false;
-        this.__tt = false;
-        this.__tm = false;
-        var state = !this.options.state;
-        this.set("state", state);
     },
 });
 })(this);
