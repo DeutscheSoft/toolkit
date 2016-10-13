8b9bee56c13c7e01eb1a7e0320c3147335d55c1d
Arne Goedeke <el@laramies.com>
2016-10-13 16:10:11 +0200

    TK.set_text: avoid textContent unless necessary
    Setting textContent always creates a new DOM node. Instead,
    if there is only one child which is a text node, we simply set
    the data property. This avoids one allocation.

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -279,14 +279,17 @@ function empty(element) {
      */
     while (element.lastChild) element.removeChild(element.lastChild);
 }
-function set_text(element, string) {
+function set_text(element, text) {
     /**
      * Sets a string as new exclusive text node of an HTMLElement.
      * @param {HTMLElement} element - The element to clean up
      * @param {string} text - The string to set as text content
      * @function TK.textContent
      */
-    element.textContent = string;
+    if (element.childNodes.length === 1 && typeof element.childNodes[0].data === "string")
+        element.childNodes[0].data = text;
+    else
+        element.textContent = text;
 }
 function html(string) {
     /**
