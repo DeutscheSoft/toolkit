8e6bd46dcb8cb18a6845b01214900daec19c8ba7
Arne Goedeke <el@laramies.com>
2017-10-02 16:08:02 +0200

    TK: make event handlers active by default
    This commit also adds support for setting 'passive' event handlers,
    if supported.

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -1089,6 +1089,65 @@ function print_widget_tree(w, depth) {
   }
 }
 
+/* Detection and handling for passive event handler support.
+ * The chrome team has threatened to make passive event handlers
+ * the default in a future version. To make sure that this does
+ * not break our code, we explicitly register 'active' event handlers
+ * for most cases.
+ */
+
+/* generic code, supports node arrays */
+function add_event_listener(e, type, cb, options) {
+    if (Array.isArray(e)) {
+        for (var i = 0; i < e.length; i++)
+            e[i].addEventListener(type, cb, options);
+    } else e.addEventListener(type, cb, options);
+}
+function remove_event_listener(e, type, cb, options) {
+    if (Array.isArray(e)) {
+        for (var i = 0; i < e.length; i++)
+            e[i].removeEventListener(type, cb, options);
+    } else e.removeEventListener(type, cb, options);
+}
+
+/* Detect if the 'passive' option is supported.
+ * This code has been borrowed from mdn */
+var passiveSupported = false;
+
+try {
+  var options = Object.defineProperty({}, "passive", {
+    get: function() {
+      passiveSupported = true;
+    }
+  });
+
+  window.addEventListener("test", null, options);
+  window.removeEventListener("test", null);
+} catch(err) {}
+
+var active_options, passive_options;
+
+if (passiveSupported) {
+  active_options = { passive: false };
+  passive_options = { passive: true };
+} else {
+  active_options = false;
+  passive_options = false;
+}
+
+function add_active_event_listener(e, type, cb) {
+  add_event_listener(e, type, cb, active_options);
+}
+function remove_active_event_listener(e, type, cb) {
+  remove_event_listener(e, type, cb, active_options);
+}
+function add_passive_event_listener(e, type, cb) {
+  add_event_listener(e, type, cb, passive_options);
+}
+function remove_passive_event_listener(e, type, cb) {
+  remove_event_listener(e, type, cb, passive_options);
+}
+
 TK = w.toolkit = {
     // ELEMENTS
     S: new DOMScheduler(),
@@ -1191,18 +1250,10 @@ TK = w.toolkit = {
     // EVENTS
 
     delayed_callback : delayed_callback,
-    add_event_listener: function(e, type, cb) {
-        if (Array.isArray(e)) {
-            for (var i = 0; i < e.length; i++)
-                e[i].addEventListener(type, cb);
-        } else e.addEventListener(type, cb);
-    },
-    remove_event_listener: function(e, type, cb) {
-        if (Array.isArray(e)) {
-            for (var i = 0; i < e.length; i++)
-                e[i].removeEventListener(type, cb);
-        } else e.removeEventListener(type, cb);
-    },
+    add_event_listener: add_active_event_listener,
+    remove_event_listener: remove_active_event_listener,
+    add_passive_event_listener: add_passive_event_listener,
+    remove_passive_event_listener: remove_passive_event_listener,
 
     // OTHER
 
