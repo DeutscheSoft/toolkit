819fc25a5caaeaa1e4940d37fce098fe53ddd8ee
Markus Schmidt <schmidt@boomshop.net>
2018-09-14 00:32:47 +0200

    Docs: filter navigation, sticky headers, title top right

--- doc/jsdoc/script.js
+++ doc/jsdoc/script.js
@@ -1,18 +1,26 @@
 (function (w) {
 var fun;
+var navsearch = {};
 w.__init__ = function () {
     var nav = document.getElementById("navigation");
     var hash = location.hash.substr(1);
+    
     if (hash == "run") {
         setTimeout(run_example, 500);
     } else {
         var match = hash.match(/nav:([0-9]*)/);
         if (match && match.length)
             nav.scrollTop = parseInt(match[1]);
-        location.hash = "";
     }
+    
     var url = w.location.pathname;
     url = url.substring(url.lastIndexOf('/')+1);
+    
+    function setscroll (e) {
+        if (!TK.get_id("nf_input").value)
+            this.href = this.href.split("#")[0] + "#nav:" + parseInt(nav.scrollTop);
+    }
+    
     if (url) {
         var c = document.querySelectorAll("nav#navigation div.navblock ul.itemsnav a");
         for (var o of c) {
@@ -22,18 +30,41 @@ w.__init__ = function () {
                 if (!match)
                     nav.scrollTop = o.offsetTop - nav.offsetHeight / 2 + o.offsetHeight / 2;
             }
-            function setscroll (e) {
-                this.href = this.href.split("#")[0] + "#nav:" + nav.scrollTop;
-            }
+            
             o.addEventListener("mousedown", setscroll);
             o.addEventListener("touchstart", setscroll);
+            var key = o.textContent.toLowerCase();
+            while (navsearch[key])
+                key += "_";
+            navsearch[key] = o;
         };
     }
+    
+    var nfinput = TK.get_id("nf_input");
+    var nfclear = TK.get_id("nf_clear");
+    nfinput.addEventListener("keyup", function () {
+        filter_nav(this.value.toLowerCase());
+    });
+    nfclear.addEventListener("click", function () {
+        nfinput.value = "";
+        filter_nav("");
+    });
+    
+    TK.get_id("h1").appendChild(document.querySelector("header h1").cloneNode(true));
 }
 window.addEventListener("load", __init__);
 
 w.prepare_example = function () {
     window.addEventListener("load", setup_example.bind(this, document.currentScript));
+}
+
+function filter_nav (str) {
+    for (var key in navsearch) {
+        var display = "block";
+        if (str && key.indexOf(str) == -1)
+            display = "none";
+        navsearch[key].parentElement.style.display = display;
+    }
 }
 
 function setup_javascript(current, code) {
