c1195c320a7cc2d9530c02613f5fd43f33f25ae1
Markus Schmidt <schmidt@boomshop.net>
2018-09-29 03:55:50 +0200

    Window: set new dimensions to Drag on resize

--- toolkit/widgets/window.js
+++ toolkit/widgets/window.js
@@ -584,6 +584,13 @@ TK.Window = TK.class({
         this.set("shrink", !this.options.shrink);
     },
     
+    resize: function () {
+        this.Drag.set("min", {x: 0 - this.options.width + 20, y: 0});
+        this.Drag.set("max", {x: TK.width() - 20, y: TK.height() - 20});
+        console.log("resize")
+        TK.Container.prototype.resize.call(this);
+    },
+    
     redraw: function () {
         var I = this.invalid;
         var O = this.options;
