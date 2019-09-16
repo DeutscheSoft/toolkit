abff3678d271eef310dca0fb4b93412a04b8dc52
Markus Schmidt <schmidt@boomshop.net>
2019-09-16 18:12:03 +0200

    Widget: docs

--- toolkit/widgets/widget.js
+++ toolkit/widgets/widget.js
@@ -98,11 +98,11 @@ TK.Widget = TK.class({
      * @param {Object} [options={ }] - An object containing initial options.
      * 
      * @property {String} [options.class=""] - A class to add to the class attribute of the main element.
-     * @property {Object} [options.container] - A container the main element shall be added to.
+     * @property {HTMLElement} [options.container] - A container the main element shall be added to.
      * @property {String} [options.id=""] - A string to be set as id attribute on the main element.
      * @property {Object} [options.styles=""] - An object containing CSS declarations to be added directly to the main element.
      * @property {Boolean} [options.disabled=false] - Toggles the class <code>toolkit-disabled</code>.
-     * @property {Object} [options.element] - An element to be used as the main element.
+     * @property {HTMLElement} [options.element] - An element to be used as the main element.
      * @property {Boolean} [options.active] - Toggles the class <code>toolkit-inactive</code>.
      * @property {Boolean} [options.needs_resize=true] - Set to true if the resize function shall be called before the next redraw.
      * @property {Boolean} [options.dblclick=400] - Set a time in milliseconds for triggering double click event. If 0, no double click events are fired.
